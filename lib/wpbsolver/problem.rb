2dfeea4cbc181aa46d2d65ce076f62751f698623
Szabolcs Seláf <selu@selu.org>
2016-12-18 01:11:35 +0100

    improve performance

--- lib/wpbsolver/problem.rb
+++ lib/wpbsolver/problem.rb
@@ -46,6 +46,44 @@ module WPBSolver
       nil
     end
 
+    def solve_better(number_of_solutions=1)
+      @count = 0
+      @results = []
+      find_solution_better([Balls.new(@ball_number)], @measure_number, [], number_of_solutions)
+      (0...@results.count).each do |number|
+        puts " -- Solution #{number+1} --"
+        display(number)
+      end
+    end
+
+    def find_solution_better(state_set, max_measures, all_measures, number_of_solutions)
+      puts "Measures: #{max_measures}, states: #{state_set.count}"
+      n = state_set.first.size
+      ids = state_set.first.ids
+      level = n/3
+      ids.combination(level*2).each do |subset|
+        reverse = []
+        subset.combination(level).each do |left|
+          next if reverse.include?(left)
+          right = subset - left
+          reverse << right
+          new_state_set = state_set.map do |bs|
+            Scale.measure(bs, left, right)
+          end.flatten
+          if max_measures == 1 && new_state_set.all? {|bs| bs.success? }
+            @results << all_measures+[{left: left, right: right, set: new_state_set}]
+            return if @results.count >= number_of_solutions
+          elsif max_measures > 1
+            if new_state_set.map(&:case_number).max <= (Scale.number_of_outcomes ** (max_measures-1))
+              find_solution_better(new_state_set, max_measures-1, all_measures+[{left: left, right: right}], number_of_solutions)
+            end
+            return if @results.count >= number_of_solutions
+          end
+        end
+      end
+      nil
+    end
+
     def display(number)
       result = @results[number]
       if result.nil?
@@ -70,14 +108,14 @@ module WPBSolver
       @count = 0
       [1, 2, 0].                       # 1: left, 2: right, 0: no
       repeated_permutation(@measure_number). # possible measures of a ball
-      select{|series| series != [0,0,0]}. # every ball should be measures at once
-      combination(@ball_number).       # choosing 12 different measures: 9657700
-      select do |mt|                   # keep only where put 4 balls onto both arms
-        mt.reduce([0]*3*@measure_number) do |r,s|
+      select{|series| series != [0]*@measure_number}. # every ball should be measures at once
+      combination(@ball_number).each do |mt|
+        # keep only where put 4 balls onto both arms
+        next unless mt.reduce([0]*3*@measure_number) do |r,s|
           s.each_with_index{|v,idx| r[v+3*idx] += 1}
           r
         end.all? {|v| v == (@ball_number/3)}
-      end.each do |mt|
+
         mt = mt.transpose
         @count += 1
         states = [Balls.new(@ball_number)]
@@ -97,8 +135,9 @@ module WPBSolver
             Scale.measure(balls, left, right)
           end.flatten
           if states.all? {|balls| balls.success?}
-            measures.last[:set] = states
+            #measures.last[:set] = states
             @results << measures
+            return if @results.count >= 1
           end
         end
       end
